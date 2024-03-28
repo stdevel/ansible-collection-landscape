1dab6be6f105c7e55021f86a9d6fece8ac58668e
Christian Stankowic <christian.stankowic@sva.de>
2024-03-28 14:05:31 +0100

    fix linting

--- roles/client/molecule/default/tests/test_default.py
+++ roles/client/molecule/default/tests/test_default.py
@@ -1,6 +1,8 @@
 """
 Molecule unit tests
 """
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 import os
 import testinfra.utils.ansible_runner
 
--- roles/server/molecule/default/tests/test_default.py
+++ roles/server/molecule/default/tests/test_default.py
@@ -1,6 +1,8 @@
 """
 Unit tests
 """
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 import os
 import testinfra.utils.ansible_runner
 
